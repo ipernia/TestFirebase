813c3bca2b99ae69b3f3232d8ae4573a75735ffd
ipernia <ignaciopernia@gmail.com>
2016-11-03 14:44:23 +0100

    Control de failure a la hora de realizar el login mediante un listener.

--- app/src/main/java/com/ipernia/testfirebase/LoginActivity.java
+++ app/src/main/java/com/ipernia/testfirebase/LoginActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -77,6 +78,12 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             finish();
                             startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
                         }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
     }
